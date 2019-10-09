1b523ec08d9ae135b2a3b97d3a331a7dd35bf397
Nour Hajar <nour@nourhajar.com>
2019-10-09 14:31:09 -0700

    first commit

---
+++ XCodeTest/XCodeTestTests/XCodeTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  XCodeTestTests.swift
+//  XCodeTestTests
+//
+//  Created by Steven Hajar on 9/24/19.
+//  Copyright © 2019 Steven Hajar. All rights reserved.
+//
+
+import XCTest
+@testable import XCodeTest
+
+class XCodeTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
